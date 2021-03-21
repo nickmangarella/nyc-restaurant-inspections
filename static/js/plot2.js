35367ec5fc8afb378cf13524c86af577d8b7802b
min xie <xm@mins-MBP.home>
2021-03-21 17:59:37 -0400

    bar plot for flask

---
+++ static/js/plot2.js
@@ -0,0 +1,73 @@
+
+d3.json("http://127.0.0.1:5000/api/v1.0/critical_flag").then((data) => {
+    var QueensCriticalY = 0;
+    var QueensCriticalN = 0;
+    var ManCriticalY = 0;
+    var ManCriticalN = 0;
+    var SICriticalY = 0;
+    var SICriticalN = 0;
+    var BronxCriticalY = 0;
+    var BronxCriticalN = 0;
+    var BrooklynCriticalY = 0;
+    var BrooklynCriticalN = 0;
+
+    data.forEach((restaurant) => {
+        if (restaurant["boro"]==="Queens" && restaurant["flag"]==="Y") {
+            QueensCriticalY += 1;
+        } else if (restaurant["boro"]==="Queens" && restaurant["flag"]==="N") {
+            QueensCriticalN += 1;
+        } else if (restaurant["boro"]==="Manhattan" && restaurant["flag"]==="Y") {
+            ManCriticalY += 1;
+        } else if (restaurant["boro"]==="Manhattan" && restaurant["flag"]==="N") {
+            ManCriticalN += 1;
+        } else if (restaurant["boro"]==="Staten Island" && restaurant["flag"]==="Y") {
+            SICriticalY += 1;
+        } else if (restaurant["boro"]==="Staten Island" && restaurant["flag"]==="N") {
+            SICriticalN += 1;
+        } else if (restaurant["boro"]==="Bronx" && restaurant["flag"]==="Y") {
+            BronxCriticalY += 1;
+        } else if (restaurant["boro"]==="Bronx" && restaurant["flag"]==="N") {
+            BronxCriticalN += 1;
+        } else if (restaurant["boro"]==="Brooklyn" && restaurant["flag"]==="Y") {
+            BrooklynCriticalY += 1;
+        } else if (restaurant["boro"]==="Brooklyn" && restaurant["flag"]==="N") {
+            BrooklynCriticalN += 1;
+        } 
+    });
+    console.log(`QueensCriticalY ${QueensCriticalY}`)
+    console.log(`QueensCriticalN ${QueensCriticalN}`)
+    console.log(`ManCriticalY ${ManCriticalY}`)
+    console.log(`ManCriticalN ${ManCriticalN}`)
+    console.log(`SICriticalY ${SICriticalY}`)
+    console.log(`SICriticalN ${SICriticalN}`)
+    console.log(`BronxCriticalY ${BronxCriticalY}`)
+    console.log(`BronxCriticalN ${BronxCriticalN}`)
+    console.log(`BrooklynCriticalY ${BrooklynCriticalY}`)
+    console.log(`BrooklynCriticalN ${BrooklynCriticalN}`)
+
+    var barData = [
+        {
+            x: ["Queens", "Manhattan", "Staten Island", "Bronx", "Brooklyn"],
+            y: [QueensCriticalY, ManCriticalY, SICriticalY, BronxCriticalY, BrooklynCriticalY],
+            type: "bar",
+            name: "Y",
+            barmode: "stack",
+            marker: {color: "#448"}
+        },
+        {
+            x: ["Queens", "Manhattan", "Staten Island", "Bronx", "Brooklyn"],
+            y: [QueensCriticalN, ManCriticalN, SICriticalN, BronxCriticalN, BrooklynCriticalN],
+            type: "bar",
+            name: "N",
+            barmode: "stack",
+            marker: {color: "#080"}
+        }];
+
+    var barLayout = {
+        title: "The Distribution of Critical Flags by Neighborhood",
+        margin: { t: 30, l: 30 }
+    };
+
+    Plotly.newPlot("bar", barData, barLayout)
+});
+
