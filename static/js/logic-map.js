b8a44e1173df32129a68f10cd06ed4c142bce93d
nickm <nickmangarella89@gmail.com>
2021-03-22 23:12:04 -0400

    fixing map id and adjusting layers

--- static/js/logic-map.js
+++ static/js/logic-map.js
@@ -1,28 +1,6 @@
-// Create map
-var myMap = L.map("rrMap", {
-  center: [40.7128, -74.0060],
-  zoom: 12,
-  layers: [ratsLayer, roachesLayer]
-});
-
-// Add the tile layer
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/dark-v9",
-  accessToken: API_KEY
-}).addTo(myMap)
-
-// Overlays that may be toggled on or off
-var overlayLayers = {
-  Rats: ratsLayer,
-  Roaches: roachesLayer
-};
-
-// Pass layers into layer control
-L.control.layers(overlayLayers).addTo(myMap);
+// Arrays to hold circles
+var ratCircles = [];
+var roachCircles = [];
 
 // Fetch the data with the flask route
 fetch("http://127.0.0.1:5000/leaflet").then(response => response.json()).then((data) => {
@@ -31,33 +9,74 @@ fetch("http://127.0.0.1:5000/leaflet").then(response => response.json()).then((d
   var rats = data.filter(x => x.Violation_Code === "04K");
   var roaches = data.filter(x => x.Violation_Code === "04M");
 
-  console.log(rat);
-  console.log(roach);
+  console.log(rats);
+  console.log(roaches);
     
 
   // For loop to go through restaurants with rats
   for (var i = 0; i < rats.length; i++) {
 
     // Render the restaurants as circles
-    var ratsLayer = L.layerGroup(L.circle([rats[i].Lat, rats[i].Long], {
-      color: "white",
-      fillColor: "#FF00FF",
-      fillOpacity: 1,
-      radius: 50,
-      weight: 1
-    })).bindPopup("<h4>" + rats[i].Name + "</h4> <hr> <p>" + rats[i].Address + "</p> <p>" + rats[i].Violation_Desc + "</p>").addTo(myMap);
+    ratCircles.push(
+      L.circle([rats[i].Lat, rats[i].Long], {
+        color: "white",
+        fillColor: "#FF00FF",
+        fillOpacity: 1,
+        radius: 50,
+        weight: 1
+      }).bindPopup("<h4>" + rats[i].Name + "</h4> <hr> <p>" + rats[i].Address + "</p> <p>" + rats[i].Violation_Desc + "</p>")
+    );
   }
 
   // For loop to go through restaurants with roaches
   for (var i = 0; i < roaches.length; i++) {
 
     // Render the restaurants as circles
-    var roachesLayer = L.layerGroup(L.circle([roaches[i].Lat, roaches[i].Long], {
-      color: "white",
-      fillColor: "#7FFFD4",
-      fillOpacity: 1,
-      radius: 50,
-      weight: 1
-    })).bindPopup("<h4>" + roaches[i].Name + "</h4> <hr> <p>" + roaches[i].Address + "</p> <p>" + roaches[i].Violation_Desc + "</p>").addTo(myMap);
+    roachCircles.push(
+      L.circle([roaches[i].Lat, roaches[i].Long], {
+        color: "white",
+        fillColor: "#7FFFD4",
+        fillOpacity: 1,
+        radius: 50,
+        weight: 1
+      }).bindPopup("<h4>" + roaches[i].Name + "</h4> <hr> <p>" + roaches[i].Address + "</p> <p>" + roaches[i].Violation_Desc + "</p>")
+    );
   }
 }).catch(err => console.error(err));
+
+// Add rat and roach cirlces to new layer groups
+var ratsLayer = L.layerGroup(ratCircles);
+var roachesLayer = L.layerGroup(roachCircles);
+
+console.log(ratCircles);
+console.log(roachCircles);
+
+// Add the tile layer
+var darkLayer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/dark-v9",
+  accessToken: API_KEY
+});
+
+var baseLayers = {
+  Dark: darkLayer
+};
+
+// Overlays that may be toggled on or off
+var overlayLayers = {
+  Rats: ratsLayer,
+  Roaches: roachesLayer
+};
+
+// Create map
+var myMap = L.map("rrMap", {
+  center: [40.7128, -74.0060],
+  zoom: 12,
+  layers: [darkLayer, ratsLayer, roachesLayer]
+});
+
+// Pass layers into layer control
+L.control.layers(baseLayers, overlayLayers).addTo(myMap);
